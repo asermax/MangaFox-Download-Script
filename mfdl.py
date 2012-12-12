dd34f40d0f09fb45f84f287660faca2c72bc49f2
Agustin Carrasco <asermax@gmail.com>
2012-12-12 01:53:10 -0300

    Made the whole chapter search and download more flexible and reliable. Chapter numbers are followed even if there isn't a good correlation with the order on the page. Also, allows specifying a volume to download through the 'volume' keyword after the manga name

--- mfdl.py
+++ mfdl.py
@@ -10,6 +10,7 @@ Updated by Agustín Carrasco <asermax@gmail.com>
 #ipshell = IPShellEmbed()
 
 import sys
+import re
 import os
 import urllib
 import glob
@@ -28,22 +29,94 @@ def get_page_soup(url):
         for line in html_file:
             html += line
     return BeautifulSoup(html)
-
-
+    
+def get_volume_chapter_number(url):
+    split = url.rsplit("/")
+    return '%g' % float(split[-3].replace('v', '')), \
+        '%g' % float(split[-2].replace('c', ''))
+            
 def get_chapter_urls(manga_name):
     """Get the chapter list for a manga"""
+    print "Getting chapter urls"
     url = "{0}manga/{1}?no_warning=1".format(URL_BASE, manga_name.lower())
     print "Url: " + url
+    
     soup = get_page_soup(url)
-    chapters = []
+    chapters = {}
     links = soup.findAll('a', {"class": "tips"})
+    
     for link in links:
-        chapters.append(link['href'].replace('1.html', ''))
+        url = link['href'].replace('1.html', '')
+        volume, chapter = get_volume_chapter_number(url)
+        
+        if chapters.has_key(volume):
+            chapters[volume][chapter] = url
+        else:
+            chapters[volume] = {chapter: url}
+        
     if(len(links) == 0):
-        print "Warning: Manga either unable to be found, or no chapters - please check the url above";
-    return list(set(chapters)) # ugly yo-yo code to remove duplicates
+        print "Warning: Manga either unable to be found, or no chapters - please check the url above";       
+        
+    return chapters
+       
+def find_volume_chapter(chapters, number):
+    matching = []
+    
+    for volume in chapters:
+        matching.extend([(volume, chapter) for chapter in chapters[volume] 
+            if chapter == number])
+                  
+    chapter = (None, None)
+      
+    if not matching:
+        print "Chapter not found"
+  
+    elif len(matching) > 1:
+        print "Warning: more than one chapter with that number, try again using the fully qualified chapter (e.g. v1c1)"
+        print 'Chapters found: ',
+        
+        for chap in matching:
+            print 'v%sc%s' % chap,
+    
+        print
+        
+    else:
+        chapter = matching[0]
+                           
+    return chapter
+    
+def strip_volume_chapter(fully_qualified):
+    match = re.search('v(\d+)c(\d+)', fully_qualified).groups()
+    chapter = (None, None)
+    
+    if match:
+        chapter = match.groups()
+    else:
+        print "Chapter not found"
+    
+    return 
 
+def is_number(string):
+    try:
+        float(string)
+        return True
+    except:
+        return False   
+ 
+def clean_input_chapter(chapters, chapter):
+    if is_number(chapter):
+        volume, chapter = find_volume_chapter(chapters, chapter)
+    else:
+        volume, chapter = strip_volume_chapter(chapter)
+        
+    return volume, chapter
 
+def get_volumes_in_range(chapters, volume_start, volume_end):
+    return [volume for volume 
+        in sorted(chapters.iteritems(), key=lambda x: float(x[0]))
+        if float(volume[0]) >= float(volume_start) 
+        and float(volume[0]) <= float(volume_end)]
+          
 def get_page_numbers(soup):
     """Return the list of page numbers from the parsed page"""
     raw = soup.findAll('select', {'class': 'm'})[0]
@@ -54,7 +127,6 @@ def get_page_numbers(soup):
         
     pages.remove('0')
     return pages
-
 
 def get_chapter_image_urls(chapter_url):
     """Find all image urls of a chapter and return them"""
@@ -70,11 +142,6 @@ def get_chapter_image_urls(chapter_url):
         images = page_soup.findAll('img', {'id': 'image'})
         image_urls.append(images[0]['src'])
     return image_urls
-
-
-def get_chapter_number(url_fragment):
-    """Parse the url fragment and return the chapter number."""
-    return ''.join(url_fragment.rsplit("/")[-2:])
 
 def check_jpg(jpeg_file):
     data = open(jpeg_file,'rb').read(11)
@@ -108,55 +175,82 @@ def makecbz(dirname):
         myzip.write(filename)
     myzip.close()
 
+def download_chapter(manga_name, volume, chapter, url):
+    name = 'v%sc%s' % (volume, chapter)
+    print("===============================================")
+    print "Chapter " + name
+    print("===============================================")
+    image_urls = get_chapter_image_urls(url)
+    download_urls(image_urls, manga_name, name)
+    download_dir = "./{0}/{1}".format(manga_name, name)
+    makecbz(download_dir)
+    shutil.rmtree(download_dir)
+
 def download_manga_range(manga_name, range_start, range_end):
     """Download a range of a chapters"""
-    print "Getting chapter urls"
-    chapter_urls = get_chapter_urls(manga_name)
-    chapter_urls.sort()
-    for url_fragment in chapter_urls[int(range_start)-1:int(range_end)+1]:
-        chapter_number = get_chapter_number(url_fragment)
-        print("===============================================")
-        print("Chapter " + chapter_number)
-        print("===============================================")
-        image_urls = get_chapter_image_urls(url_fragment)
-        download_urls(image_urls, manga_name, chapter_number)
-        download_dir = "./{0}/{1}".format(manga_name, chapter_number)
-        makecbz(download_dir)
-        shutil.rmtree(download_dir)
+    volumes = get_chapter_urls(manga_name)
+    
+    volume_start, chapter_start = clean_input_chapter(volumes, range_start)
+    volume_end, chapter_end = clean_input_chapter(volumes, range_end)
+
+    if volume_start and volume_end:     
+        for volume, chapters in get_volumes_in_range(volumes, volume_start,
+            volume_end):
+            for chapter, url in [chapter for chapter 
+                in sorted(chapters.iteritems(), key=lambda x: float(x[0]))
+                if float(chapter[0]) >= float(chapter_start)
+                and float(chapter[0]) <= float(chapter_end)]:
+                download_chapter(manga_name, volume, chapter, url)
+            
+        os.remove("page.html")
+
+def download_manga_volume(manga_name, volume_start, volume_end=None):       
+    volumes = get_chapter_urls(manga_name)
+    
+    if not volume_start.isdigit():
+        volume_start = volume_start.replace('v', '')
+            
+    if not volume_end:
+        volume_end = volume_start
+    elif not volume_end.isdigit():
+        volume_end = volume_end.replace('v', '')
+        
+    for volume, chapters in get_volumes_in_range(volumes, volume_start,
+        volume_end):
+        for chapter, url in sorted(chapters.iteritems(),
+            key=lambda x: float(x[0])):
+
+            download_chapter(manga_name, volume, chapter, url)   
+    
     os.remove("page.html")
-
-
+    
 def download_manga(manga_name, chapter_number=None):
     """Download all chapters of a manga"""
-    chapter_urls = get_chapter_urls(manga_name)
-    chapter_urls.sort()
+    volumes = get_chapter_urls(manga_name)
+    
     if chapter_number:
-        url_fragment = chapter_urls[int(chapter_number)-1]
-        chapter_number = get_chapter_number(url_fragment)
-        print("===============================================")
-        print("Chapter " + chapter_number)
-        print("===============================================")
-        image_urls = get_chapter_image_urls(url_fragment)
-        download_urls(image_urls, manga_name, chapter_number)
-        download_dir = "./{0}/{1}".format(manga_name, chapter_number)
-        makecbz(download_dir)
-        shutil.rmtree(download_dir)
+        volume, chapter = clean_input_chapter(volumes, chapter_number)
+                   
+        if volume:
+            url = chapters[volume][chapter]
+            download_chapter(manga_name, volume, chapter, url)
     else:
-        for url_fragment in chapter_urls:
-            chapter_number = get_chapter_number(url_fragment)
-            print("===============================================")
-            print("Chapter " + chapter_number)
-            print("===============================================")
-            image_urls = get_chapter_image_urls(url_fragment)
-            download_urls(image_urls, manga_name, chapter_number)
-            download_dir = "./{0}/{1}".format(manga_name, chapter_number)
-            makecbz(download_dir)
-            shutil.rmtree(download_dir)
+        for volume, chapters in sorted(volumes.iteritems(),
+            key=lambda x: float(x[0])):
+            for chapter, url in sorted(chapters.iteritems(),
+                key=lambda x: float(x[0])):
+                download_chapter(manga_name, volume, chapter, url)
+            
     os.remove("page.html")
 
 if __name__ == '__main__':
-    if len(sys.argv) == 4:
-        download_manga_range(sys.argv[1], sys.argv[2], sys.argv[3])
+    if len(sys.argv) == 5:
+        download_manga_volume(sys.argv[1], sys.argv[3], sys.argv[4])
+    elif len(sys.argv) == 4:
+        if sys.argv[2].lower() == 'volume':
+            download_manga_volume(sys.argv[1], sys.argv[3])
+        else:
+            download_manga_range(sys.argv[1], sys.argv[2], sys.argv[3])
     elif len(sys.argv) == 3:
         download_manga(sys.argv[1], sys.argv[2])
     elif len(sys.argv) == 2:
